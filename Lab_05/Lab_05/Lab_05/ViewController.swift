a1e165a878c4f2f8f93511d3f10811aec46cb2f5
Shan-shihan-Conestoga <Sbaskaran6694@conestogac.on.ca>
2023-10-13 22:38:57 -0400

    initial commit

---
+++ Lab_05/Lab_05/Lab_05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_05
+//
+//  Created by Shaik Mathar Syed on 13/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
